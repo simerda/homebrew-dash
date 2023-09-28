17344801a38ee581c517b376c96d9305c2c665ec
Jan Šimerda <simerda.jan@gmail.com>
2023-09-28 22:13:37 +0200

    nfc: disable cookies

--- src/main/java/cz/jansimerda/homebrewdash/rest/config/SecurityConfig.java
+++ src/main/java/cz/jansimerda/homebrewdash/rest/config/SecurityConfig.java
@@ -4,17 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authorization) -> authorization.anyRequest().permitAll());
         http.httpBasic(AbstractHttpConfigurer::disable);
         http.csrf(AbstractHttpConfigurer::disable);
+        http.sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 
