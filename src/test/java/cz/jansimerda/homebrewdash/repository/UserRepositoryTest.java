971e42a34dac271efa3df3d0f01978f73edaf2ea
Jan Šimerda <simerda.jan@gmail.com>
2023-10-24 00:59:46 +0200

    nfc: cleanup UserRepository tests by extending AbstractTest

--- src/test/java/cz/jansimerda/homebrewdash/repository/UserRepositoryTest.java
+++ src/test/java/cz/jansimerda/homebrewdash/repository/UserRepositoryTest.java
@@ -1,5 +1,6 @@
 package cz.jansimerda.homebrewdash.repository;
 
+import cz.jansimerda.homebrewdash.AbstractTest;
 import cz.jansimerda.homebrewdash.model.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,7 +10,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.util.UUID;
 
 @DataJpaTest
-class UserRepositoryTest {
+class UserRepositoryTest extends AbstractTest {
 
     @Autowired
     UserRepository userRepository;
@@ -17,68 +18,56 @@ class UserRepositoryTest {
     @Test
     void getFirstByEmailOrUsername() {
         // init
-        User user = createUser();
+        User user = createDummyUser();
 
         // persist
         userRepository.save(user);
 
         // test
-        Assertions.assertTrue(userRepository.getFirstByEmailOrUsername("dummy@example.com", "uname").isPresent());
-        Assertions.assertTrue(userRepository.getFirstByEmailOrUsername("other@mail.com", "dummyUser").isPresent());
+        Assertions.assertTrue(userRepository.getFirstByEmailOrUsername("duser@mail.com", "uname").isPresent());
+        Assertions.assertTrue(userRepository.getFirstByEmailOrUsername("other@mail.com", "user123").isPresent());
         Assertions.assertTrue(userRepository.getFirstByEmailOrUsername("other@mail.com", "uname").isEmpty());
     }
 
     @Test
     void getFirstByEmailOrUsernameExceptId() {
         // init
-        User user = createUser();
+        User user = createDummyUser();
 
         // persist
-        userRepository.save(user);
+        user = userRepository.save(user);
 
         // test different id
-        Assertions.assertTrue(userRepository.getFirstByEmailOrUsernameExceptId("dummy@example.com", "uname", UUID.randomUUID()).isPresent());
-        Assertions.assertTrue(userRepository.getFirstByEmailOrUsernameExceptId("other@mail.com", "dummyUser", UUID.randomUUID()).isPresent());
+        Assertions.assertTrue(userRepository.getFirstByEmailOrUsernameExceptId("duser@mail.com", "uname", UUID.randomUUID()).isPresent());
+        Assertions.assertTrue(userRepository.getFirstByEmailOrUsernameExceptId("other@mail.com", "user123", UUID.randomUUID()).isPresent());
 
         // test same id
-        Assertions.assertTrue(userRepository.getFirstByEmailOrUsernameExceptId("dummy@example.com", "uname", user.getId()).isEmpty());
-        Assertions.assertTrue(userRepository.getFirstByEmailOrUsernameExceptId("other@mail.com", "dummyUser", user.getId()).isEmpty());
+        Assertions.assertTrue(userRepository.getFirstByEmailOrUsernameExceptId("duser@mail.com", "uname", user.getId()).isEmpty());
+        Assertions.assertTrue(userRepository.getFirstByEmailOrUsernameExceptId("other@mail.com", "user123", user.getId()).isEmpty());
         Assertions.assertTrue(userRepository.getFirstByEmailOrUsernameExceptId("other@mail.com", "uname", user.getId()).isEmpty());
     }
 
     @Test
     void getFirstByEmail() {
         // init
-        User user = createUser();
+        User user = createDummyUser();
 
         // persist
         userRepository.save(user);
         // test
-        Assertions.assertTrue(userRepository.getFirstByEmail("dummy@example.com").isPresent());
+        Assertions.assertTrue(userRepository.getFirstByEmail("duser@mail.com").isPresent());
         Assertions.assertTrue(userRepository.getFirstByEmail("other@mail.com").isEmpty());
     }
 
     @Test
     void getFirstByUsername() {
         // init
-        User user = createUser();
+        User user = createDummyUser();
 
         // persist
         userRepository.save(user);
         // test
-        Assertions.assertTrue(userRepository.getFirstByUsername("dummyUser").isPresent());
+        Assertions.assertTrue(userRepository.getFirstByUsername("user123").isPresent());
         Assertions.assertTrue(userRepository.getFirstByUsername("uname").isEmpty());
-    }
-
-    private User createUser() {
-        User user = new User();
-        user.setEmail("dummy@example.com");
-        user.setPassword("(dummyPassword332)");
-        user.setUsername("dummyUser");
-        user.setFirstName("Dummy");
-        user.setSurname("User");
-        user.setIsAdmin(false);
-
-        return user;
     }
 }
